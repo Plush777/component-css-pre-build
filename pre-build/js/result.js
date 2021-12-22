38d790bf537545593f76f69d2e0ecff92f257580
Plush777 <sky11916@naver.com>
2021-12-22 14:13:35 +0900

    gnb페이지 추가

--- pre-build/js/result.js
+++ pre-build/js/result.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    
+
+    /* gnb */
+	$('.gnbHover #gnb').hover(function(){
+        $('#menuBg,.depthTwo').stop().slideDown();
+    },function(){
+		$('#menuBg,.depthTwo').stop().slideUp();
+    });
+
+    $('.gnb-li-hover #gnb>ul>li').on({
+		"mouseenter":function (){
+			$('#menuBg,.depthTwo').stop().slideDown();
+		}
+	})
+
+	$('.gnb-li-hover #gnb').on({
+		"mouseleave":function (){
+			$('#menuBg,.depthTwo').stop().slideUp();
+		}
+	});
+
+    $('.gnb-li-hover #gnb').on({
+		"mouseleave":function (){
+			$('#menuBg,.depthTwo').stop().slideUp();
+		}
+	});
+
+    $('.gnbFind #gnb>ul>li').hover(function(){
+        $(this).find('.depthTwo').stop().slideDown();
+    },function(){
+        $(this).find('.depthTwo').stop().slideUp();
+    });
 });

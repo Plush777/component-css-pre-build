a92a7ebc60ca0f3ff7a4611845329c13540558f3
Plush777 <sky11916@naver.com>
2022-01-09 22:19:46 +0900

    add : popup 페이지 추가

--- pre-build/js/component/popup.js
+++ pre-build/js/component/popup.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
+    $('#frameBody .wrap.pre.popShowHide .btnShowHide').on({
+        "click":function(){
+            $('.popup').show();
+            $('.exDimmed').show();
+        }
+    });
 
+    $('.btnClose').on({
+        "click":function(){
+            $('.popup').hide();
+            $('.exDimmed').remove();
+        }
+    });
+
+    /* 예제에 사용될 코드
+        $('#exToast').fadeIn();
+        setTimeout(function(){
+            $('#exToast').fadeOut();
+        },4000);
+    */
+    $('.btnResult').on({
+        "click":function(){
+            if($('#frameBody').css('display') == 'block'){
+                $('#exToast').fadeIn();
+                setTimeout(function(){
+                    $('#exToast').fadeOut();
+                },4000);
+            }
+        }
+    });
+
+    $('#drag').draggable({
+        cancel:'.cont',
+        containment: "window"
+    });
 });

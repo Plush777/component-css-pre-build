0054d88bb7a9074e457e5ec6b43580530b585293
Plush777 <sky11916@naver.com>
2022-02-03 23:44:32 +0900

    다크모드 작업중2

--- pre-build/js/darkmode.js
+++ pre-build/js/darkmode.js
@@ -1,39 +1,35 @@
-const $checkbox = document.querySelector('.btnDarkMode');
-const isUserColorTheme = localStorage.getItem('color-theme');
-const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-const getUserTheme = () => isUserColorTheme ? isUserColorTheme : isOsColorTheme;
-console.log(isOsColorTheme);
-const lightActive = () => {
-    $checkbox.classList.add('lightActive');
-    $checkbox.classList.remove('darkActive');
-}
+const darkModeEvent = document.querySelector('.btnDarkMode');
+const choiceTheme = localStorage.getItem('theme');
+let prefersTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const getPrefersTheme = () => choiceTheme ? choiceTheme : prefersTheme;
+const domEle = document.documentElement;
+
+window.onload = () => {
+    if (getPrefersTheme === 'dark') {
+        domEle.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        domEle.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+};
 
 const darkActive = () => {
-    $checkbox.classList.add('darkActive');
-    $checkbox.classList.remove('lightActive');
+    darkModeEvent.classList.add('active');
 }
 
-window.onload = function () {
-    if (getUserTheme === 'dark') {
-        localStorage.setItem('color-theme', 'dark');
-        document.documentElement.setAttribute('color-theme', 'dark');
+const darkDeactive = () => {
+    darkModeEvent.classList.remove('active');
+}
+    
+darkModeEvent.addEventListener('click', () => {
+    if (domEle.getAttribute('data-theme') === 'dark') {
+        domEle.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        darkDeactive();
     } else {
-        localStorage.setItem('color-theme', 'light');
-        document.documentElement.setAttribute('color-theme', 'light');
-    }
-}
-
-$checkbox.addEventListener('click', () => {
-    if (getUserTheme === 'light') {
-        lightActive();
-    } else if (getUserTheme === 'dark') {
+        domEle.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkActive();
-    }
-    if ($checkbox.classList.contains('active')) {
-        localStorage.setItem('color-theme', 'light');
-        document.documentElement.setAttribute('color-theme', 'dark');
-    } else {
-        localStorage.setItem('color-theme', 'dark');
-        document.documentElement.setAttribute('color-theme', 'dark');
     }
 });
